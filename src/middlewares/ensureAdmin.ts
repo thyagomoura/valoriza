c701a42302756f4ee30a5fa136244c2422c24972
thyago <thyagohenriquemoura@gmail.com>
2021-06-23 18:06:45 -0300

    Tratamento de erros, criaçao de middlewares, ciraçao da tabela de tags e configuraçao de controllers, entidades, router, repositorios

---
+++ src/middlewares/ensureAdmin.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from "express"
+
+
+export function ensureAdmin( 
+    request: Request, 
+    response: Response, 
+    next: NextFunction){
+
+    //verificar se o usuario e admin para
+    const admin = true;
+
+    if(admin){
+        return next();
+    }
+
+    return response.status(401).json({
+        error: "Unauthorized",
+    });
+}
