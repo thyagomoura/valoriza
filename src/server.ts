69412703b8efb7242df09899a68e0f1b5d51b026
thyago <thyagohenriquemoura@gmail.com>
2021-06-22 21:49:12 -0300

    Todas alteraçoes de rotas, services, orm, controller e entidades feitos

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,13 @@
+import "reflect-metadata";
 import express, { request } from "express";
+import "./database";
+import {router} from "./routes";
 
-//@types/express
 const app = express();
-/**
- * GET => buscar uma informação
- * POST => inserir uma informação(criaçao/algo novo)
- * PUT => alterar uma informação
- * DELETE => delete uma informação
- * PATcH => alterar uma informação especifica
- */
 
-app.get("/test", (request, response) => {
-    //request => tudo que esta entrando
-    //response => tudo que esta saindo
-    return response.send("OLA Funciona O GET");
-})
+app.use(express.json())
 
-
-app.post("/test-post", (request, response) => {
-    return response.send("OLA funciona O POST");
-})
+app.use(router);
 
 //http://localhost:5000
 app.listen(5000, () => console.log("Server is running"));
